b09bccc63980d7f32727c75660514a7e80e9ea0c
Pavel <pa11ady@yandex.ru>
2025-10-20 16:31:19 +0300

    Meal check Owner

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -11,15 +11,22 @@ public class Meal extends AbstractBaseEntity {
 
     private final int calories;
 
-    public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, dateTime, description, calories);
+    public Integer getUserId() {
+        return userId;
     }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+    private final Integer userId;
+
+    public Meal(LocalDateTime dateTime, String description, int calories, Integer userId) {
+        this(null, dateTime, description, calories, userId);
+    }
+
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories, Integer userId) {
         super(id);
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+        this.userId = userId;
     }
 
     public LocalDateTime getDateTime() {
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -5,9 +5,11 @@ import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
     private final Map<Integer, Meal> mealsMap = new ConcurrentHashMap<>();
@@ -21,26 +23,37 @@ public class InMemoryMealRepository implements MealRepository {
     public Meal save(Meal meal) {
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            mealsMap.put(meal.getId(), meal);
-            return meal;
+        } else if (!isOwner(meal.getUserId(), meal.getId())) {
+            return null;
         }
-        // handle case: update, but not present in storage
-        return mealsMap.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        mealsMap.put(meal.getId(), meal);
+        return meal;
+    }
+
+    private boolean isOwner(int userId, int mealId) {
+        Meal meal = mealsMap.get(mealId);
+        return meal != null && userId == meal.getUserId();
     }
 
     @Override
-    public boolean delete(int id) {
-        return mealsMap.remove(id) != null;
+    public boolean delete(int id, int userId) {
+        return isOwner(userId, id) && mealsMap.remove(id) != null;
     }
 
     @Override
-    public Meal get(int id) {
+    public Meal get(int id, int userId) {
+        if (!isOwner(userId, id)) {
+            return null;
+        }
         return mealsMap.get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return mealsMap.values();
+    public Collection<Meal> getAll(int userId) {
+        return mealsMap.values().stream()
+                .filter(meal -> meal.getUserId() == userId)
+                .sorted(Comparator.comparing(Meal::getDateTime, Comparator.reverseOrder()))
+                .collect(Collectors.toList());
     }
 }
 
