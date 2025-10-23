9c435ec18a6fd9589dc430b60e77ee4b2702151d
Pavel <pa11ady@yandex.ru>
2025-10-23 15:39:54 +0300

    fix 2

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -1,18 +1,13 @@
 package ru.javawebinar.topjava.util;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime start, LocalTime end) {
-        return (start == null || !lt.isBefore(start)) && (end == null || lt.isBefore(end));
-    }
-
-    public static boolean isBetweenHalfOpen(LocalDateTime value, LocalDateTime start, LocalDateTime end) {
-        return (start == null || !value.isBefore(start)) && (end == null || value.isBefore(end));
+    public static <T extends Comparable<? super T>> boolean isBetweenHalfOpen(T value, T start, T end) {
+        return (start == null || value.compareTo(start) >= 0) && (end == null || value.compareTo(end) < 0);
     }
 
     public static String toString(LocalDateTime ldt) {
