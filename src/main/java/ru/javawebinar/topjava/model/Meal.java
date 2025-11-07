2bec4306fb481db7d9d6dad226446feeec619d1e
Pavel <pa11ady@yandex.ru>
2025-11-07 17:28:04 +0300

    Meal JPA

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,41 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "Meal.UPDATE",
+                query = "UPDATE Meal m SET m.description =:description, m.calories =:calories, m.dateTime =:dateTime " +
+                        "WHERE m.id =:id AND m.user.id =:userId"),
+        @NamedQuery(name = "Meal.DELETE", query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = "Meal.ALL_SORTED", query = "SELECT m FROM Meal m WHERE m.user.id=:userId " +
+                "ORDER BY m.dateTime DESC"),
+        @NamedQuery(
+                name = "Meal.GET_BETWEEN",
+                query = "SELECT m FROM Meal m WHERE m.user.id =:userId AND m.dateTime >=:startDateTime AND " +
+                        "m.dateTime <:endDateTime ORDER BY m.dateTime DESC ")
+})
+@Table(name = "meal")
 public class Meal extends AbstractBaseEntity {
+    @Column(name = "date_time", nullable = false)
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    @Size(max = 256)
     private String description;
 
+    @Column(name = "calories", nullable = false)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @NotNull
     private User user;
 
     public Meal() {
