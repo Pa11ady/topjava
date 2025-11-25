d79bef125ec88762ae70341519e189947b6e3681
Pavel <pa11ady@yandex.ru>
2025-11-25 23:57:55 +0300

    Fix 2

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -1,15 +1,17 @@
 package ru.javawebinar.topjava.service.jdbc;
 
 import org.junit.Ignore;
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
-@Ignore("Not supported for JDBC")
 public class JdbcMealServiceTest extends AbstractMealServiceTest {
     @Override
+    @Ignore("Not supported for JDBC")
+    @Test
     public void createWithException() {
     }
 }
--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,15 +1,17 @@
 package ru.javawebinar.topjava.service.jdbc;
 
 import org.junit.Ignore;
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
-@Ignore("Not supported for JDBC")
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
     @Override
+    @Ignore("Not supported for JDBC")
+    @Test
     public void createWithException() {
     }
 }
